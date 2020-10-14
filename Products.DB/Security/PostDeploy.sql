4002ab1450d30fa2b78b0bfe2ae43638da95bf41
Duarte <mduart639@emea.comcast.com>
2020-10-14 19:23:03 +0100

    added docker support and monitoring using promotheus and grafana

---
+++ Products.DB/Security/PostDeploy.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT * FROM master.dbo.syslogins WHERE loginname = 'DockerUser' AND dbname = 'Products')
+	CREATE LOGIN DockerUser WITH PASSWORD = 's[qfBKS/r((2Cg+m' ,
+		CHECK_EXPIRATION = OFF,
+		DEFAULT_DATABASE = [Products]  
+
+CREATE USER [DockerUser] FOR LOGIN [DockerUser];
+GO
+
+
+ALTER ROLE [db_datawriter] ADD MEMBER [DockerUser];
+GO
+
+
+ALTER ROLE [db_datareader] ADD MEMBER [DockerUser];
+GO
