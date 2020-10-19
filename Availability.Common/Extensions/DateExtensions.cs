f42f0d466d908290271bee5452fc466520c0d701
Duarte <mduart639@emea.comcast.com>
2020-10-19 21:42:22 +0100

    missed file

---
+++ Availability.Common/Extensions/DateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Availability.Common.Extensions
+{
+    using System.Globalization;
+
+    public static class DateExtensions
+    {
+        public static DateTime Convert(int date)
+        {
+            return DateTime.ParseExact(date.ToString(), "ddMMyyyy", CultureInfo.InvariantCulture); ;
+        }
+    }
+}
